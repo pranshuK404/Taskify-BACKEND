f7016bf68ed61397fa2e61abf8d6182189a15df5
pranshu kumar <pranshuk1174@gmail.com>
2026-03-16 16:05:32 +0530

    feat: add MongoDB connection setup in db.js and integrate it in index.js

---
+++ src/index.js
@@ -0,0 +1,24 @@
+process.on("uncaughtException", (err) => {
+  console.error("Uncaught Exception:", err)
+  process.exit(1)
+})
+
+import "dotenv/config"
+import connectDB from "./config/connectDB.js"
+import app from "./app.js"
+
+const PORT = process.env.PORT || 8000
+
+connectDB()
+  .then(() => {
+    const server = app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`)
+    })
+
+    process.on("unhandledRejection", (err) => {
+      console.error("Unhandled Rejection:", err)
+      server.close(() => {
+        process.exit(1)
+      })
+    })
+  })
